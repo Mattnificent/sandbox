09c999278d09b12467e44d7cf3311ea9d87fb5be
m <mattbeckondeck@gmail.com>
2015-03-21 22:10:49 -0400

    7-4 Show a loading screen while waiting to login PLUS comments

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -7,21 +7,35 @@ Router.configure({
 
 Router.route('/', {name: 'postsList'});
 
-Router.route('/posts/:_id', {  
-	name: 'postPage',
-	data: function() { return Posts.findOne(this.params._id); }
+Router.route('/posts/:_id', {
+	
+  name: 'postPage',
+
+  // Return the first post with the post id 
+	data: function() { 
+    return Posts.findOne(this.params._id); 
+  }
+
 });
 
-Router.route('/submit', {name: 'postSubmit'});
+Router.route('/submit', { name: 'postSubmit'});
 
+// Hook to render accessDenied if not user
 var requireLogin = function() {
   if (! Meteor.user()) {
-    this.render('accessDenied');
-  } 
+    // To avoid access denied while login occurs
+    if (Meteor.loggingIn()) {
+      this.render(this.loadingTemplate);
+    }
+    else {
+      this.render('accessDenied');
+    } 
+  }
   else {
     this.next();
   }
 }
+
 
 Router.onBeforeAction('dataNotFound', {only: 'postPage'});
 Router.onBeforeAction(requireLogin, {only: 'postSubmit'});
