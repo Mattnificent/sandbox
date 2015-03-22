51c06b501c2d69c01857a9b443f195b24e1a792d
m <mattbeckondeck@gmail.com>
2015-03-22 00:14:08 -0400

    7-8 Sort posts by submitted timestamp.

--- microscope/client/templates/posts/posts_list.js
+++ microscope/client/templates/posts/posts_list.js
@@ -2,6 +2,7 @@
 //    posts Helper
 
 Template.postsList.helpers({
-    posts: function() {    return Posts.find();  }
+  posts: function() {
+    return Posts.find({}, {sort: {submitted: -1}});
+  }
 });
-
